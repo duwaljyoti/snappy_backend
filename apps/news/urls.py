6dae99fede90f7c8807248764a599aaa3fbbd983
Jyoti <jyoti.duwal@outcodesoftware.com>
2024-09-24 08:32:13 +0545

    Updated test URLS.

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('my-view/', views.my_view, name='my_view'),
-    path('check-health/', views.my_view, name='check_health'),
+    path('check-health/', views.check_heath, name='check_health'),
 ]
