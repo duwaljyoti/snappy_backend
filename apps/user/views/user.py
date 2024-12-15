ab4068f5f2e451fc10996c83191a156633fca15d
Jyoti <jyoti.duwal@outcodesoftware.com>
2024-12-15 13:51:40 +0545

    Also returned access token from the

--- apps/user/views/user.py
+++ apps/user/views/user.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.user.models import UserPortal
 from apps.user.serializers import user
@@ -21,13 +22,18 @@ class UserPortalViewSet(viewsets.ModelViewSet):
 
 class SignInView(APIView):
     def post(self, request):
-        user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
+        logging_user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
 
         if user:
+            refresh = RefreshToken.for_user(logging_user)
+            access = refresh.access_token
+
             return Response({
                 'message': 'Login successful',
-                'username': user.username
+                'username': logging_user.username,
+                'access': str(access),
+                'refresh': str(refresh),
             }, status=status.HTTP_200_OK)
 
             # If authentication fails, return an error response
-            return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
