30ed15dd34d261db29c951517b0c236ceeb74c4f
jyoti_duwal_oc <jyoti.duwal@outcodesoftware.com>
2026-09-12 20:02:52 +0545

    Fixed the broken URL.

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('my-view/', views.my_view, name='my_view'),
-    path('check-health/', views.check_heath, name='check_health'),
+    path('check-health/', views.check_health, name='check_health'),
     path('send-test-email/', views.send_test_mail, name='send_test_email'),
     path('burn-cpu/', views.cpu_burn, name='burn_cpu'),
     path('get_news_image/', views.get_image_urls, name='get_news_image'),
